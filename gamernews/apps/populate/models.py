ba6ca972588c59f2ba563970dcc770e68b8a8cfc
Tyler <underlost@gmail.com>
2014-03-20 13:28:30 -0500

    restructure

--- gamernews/apps/populate/models.py
+++ gamernews/apps/populate/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.template.defaultfilters import slugify
 
 APPROVED_FEED='A'
 DENIED_FEED='D'
